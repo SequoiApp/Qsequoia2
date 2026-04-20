7642640bd26b484fb3df13fad57bbaa03fcecc51
Alexandre Le Bars <alexlb329@gmail.com>
2026-04-20 21:11:43 +0200

    add get_pf and sspf_list functions

--- qsequoia2/modules/table_check/data_table.py
+++ qsequoia2/modules/table_check/data_table.py
@@ -1,6 +1,7 @@
 from qgis.core import *
 from PyQt5.QtCore import QVariant
 from pathlib import Path
+from ..utils.seq_config import *
 import processing
 
 # ==============================================
@@ -77,11 +78,53 @@ def getFinaldata(synthese):
 def sspf_surface_calculation(ua_layer) -> str:
 
     feats = list(ua_layer.getSelectedFeatures())
-    if not feats: return "0 ha"
+    if not feats: 
+        surf = sum(f.geometry().area() for f in ua_layer.getFeatures()) / 10000
+        return f"{round(surf,4)} ha"
     tmp = QgsVectorLayer("Polygon?crs=" + ua_layer.crs().authid(), "tmp", "memory")
     tmp.dataProvider().addFeatures(feats)
     surf = sum(f.geometry().area() for f in tmp.getFeatures()) / 10000
     return f"{round(surf,4)} ha"
+
+
+
+def get_pf_list(ua_layer) -> list:
+    """return the list of all pf name (str)"""
+
+    field = seq_field("pcl_code")["name"]
+    values = ua_layer.uniqueValues(ua_layer.fields().indexOf(field))
+
+    return [v for v in values if v.isdigit()]
+
+
+def get_sspf_list(ua_layer, pf_list=None) -> list :
+    """return the list of sspf by pf, if pf_list is None return all sspf of the layer"""
+    
+    sspf_field = seq_field("sub_code")["name"]
+    pf_field = seq_field("pcl_code")["name"]
+    
+    if pf_list is None:
+        pf_list = get_pf_list(ua_layer)
+
+    sspf_list = []
+
+    for pf in pf_list: 
+        sub_values = {f[sspf_field]for f in ua_layer.getFeatures()if str(f[pf_field]) == pf}
+        
+        for d in sub_values:
+            sspf_list.append(d)
+            
+    return sspf_list
+
+
+
+
+
+
+
+
+
+
 
     
 
